b05088da0f39517552f0ebc42f63d38b8b5f6c25
jackarivera <jackarivera@gmail.com>
2022-06-14 12:25:42 -0500

    Added ROS2 dependencies and functionality

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install python-pip -y
 sudo apt-get install python3-pip -y
 sudo apt-get install screen -y
 sudo apt-get install nano -y
@@ -8,10 +7,8 @@ pip3 install console-menu
 pip3 install boto3
 pip3 install pexpect
 pip install six
-cp -rf ../rover_tools ~/
-git clone https://github.com/roverrobotics/robottests ~/rover_tools/testing/robottests/
 cwd=$(pwd)
 cf="${cwd}/../credentials.json"
-python3 ~/rover_tools/menu/menu_main.py --cf ${cf}
-source /opt/ros/melodic/setup.bash
-source ~/catkin_ws/devel/setup.bash
+python3 ~/rover_tools_ros2/menu/menu_main.py --cf ${cf}
+source /opt/ros/humble/setup.bash
+source ~/rover_workspace/install/setup.sh
